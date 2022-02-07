cf1aadfad9f2a67d220c242251272bf584e6966e
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-02-07 12:40:06 +0100

    assigment_7_sum_average_Numbers

--- U1 (01.02.2022)/Averager/7) Function that updates the results/index.js
+++ U1 (01.02.2022)/Averager/7) Function that updates the results/index.js
@@ -1,5 +1,4 @@
-"use strict";
-
+'use strict'
 
 /*
 
@@ -19,42 +18,59 @@ each time a number is selected or deselected.
 
 */
 function createNumberDiv (gridContainer) {
-let numberDiv = document.createElement ("div");
-numberDiv.innerHTML = randomNumber(100);
-numberDiv.addEventListener("click", function (){numberDiv.classList.toggle('selected')});
-document.querySelector('#grid').appendChild(numberDiv);
-gridContainer.appendChild(numberDiv);
+  let numberDiv = document.createElement('div')
+  numberDiv.innerHTML = randomNumber(100)
+  numberDiv.addEventListener('click', function () {
+    numberDiv.classList.toggle('selected')
+    updateResults('selected')
+  })
+  document.querySelector('#grid').appendChild(numberDiv)
+  gridContainer.appendChild(numberDiv)
 
-return numberDiv;
+  return numberDiv
 }
 
 function randomNumber (max) {
-return Math.floor(max * Math.random())
+  return Math.floor(max * Math.random())
 }
 
 function gridMaker (gridContainer, R, C) {
   gridContainer.style.gridTemplateColumns = `repeat(${C}, 1fr)`
   gridContainer.style.gridTemplateRows = `repeat(${R}, + 1fr)`
-  gridContainer.style.display = "grid"
-  gridContainer.innerHTML = "";
-  
+  gridContainer.style.display = 'grid'
+  gridContainer.innerHTML = ''
+
+  document.querySelector('#selected span').innerHTML = ''
+  document.querySelector('#amount span').innerHTML = 0
+  document.querySelector('#sum span').innerHTML = 0
+  document.querySelector('#average span').innerHTML = ''
+
   // for (let i = 0; i < R; i++) {
-    //   for (let J = 0; J < C; J++) {
-      //     let numberDiv = document.createElement('div')
-      //     numberDiv.innerHTML = randomNumber(100)
-      
-      //     gridContainer.appendChild(numberDiv)
-      //   }
-      // }
-      for (let i = 0;i < R*C; i++){
-        createNumberDiv(gridContainer)
+  //   for (let J = 0; J < C; J++) {
+  //     let numberDiv = document.createElement('div')
+  //     numberDiv.innerHTML = randomNumber(100)
 
-      }
-    }
+  //     gridContainer.appendChild(numberDiv)
+  //   }
+  // }
+  for (let i = 0; i < R * C; i++) {
+    createNumberDiv(gridContainer)
+  }
+}
 
-document.querySelector("button").addEventListener("click", function(){gridMaker(document.querySelector("#grid"),document.querySelector("#inputRows").value,document.querySelector("#inputCols").value)})
+document.querySelector('button').addEventListener('click', function () {
+  gridMaker(
+    document.querySelector('#grid'),
+    document.querySelector('#inputRows').value,
+    document.querySelector('#inputCols').value
+  )
+})
 
-gridMaker(document.querySelector("#grid"),document.querySelector("#inputRows").value,document.querySelector("#inputCols").value)
+gridMaker(
+  document.querySelector('#grid'),
+  document.querySelector('#inputRows').value,
+  document.querySelector('#inputCols').value
+)
 /*
 
 The idea is to use the functions we created at the beginning (adder, averg) to
@@ -76,30 +92,52 @@ The function getArrayOfSelectedNumbers does the following:
 */
 
 function getArrayOfSelectedNumbers (className) {
-
-  // This weird line creates an array with all the numberDivs that have the 
-  // class className. Naturally, when you call this function, you will need 
+  // This weird line creates an array with all the numberDivs that have the
+  // class className. Naturally, when you call this function, you will need
   // to assign a value to className. What value should that be, do you think?
   // The reference to the array is stored in arrayElements
-  let arrayElements = Array.from(document.querySelectorAll("." + className));
+  let arrayElements = Array.from(document.querySelectorAll('.' + className))
 
   // Create a new array and store its reference in arrayNumbers
-  let arrayNumbers = [];
+  let arrayNumbers = []
 
   // Iterate through all the elements in arrayElements.
   // For each numberDiv in arrayElements create a new element in arrayNumbers
   // with the numeric value of the random number.
   for (let i = 0; i < arrayElements.length; i++) {
-    let numberAsString = arrayElements[i].innerHTML;
-    let number = parseInt(numberAsString);
-    arrayNumbers.push(number);
+    let numberAsString = arrayElements[i].innerHTML
+    let number = parseInt(numberAsString)
+    arrayNumbers.push(number)
   }
 
   // Make the array of numbers available outside the function
-  return arrayNumbers;
-
+  return arrayNumbers
 }
 
+function adder_infinite (_array) {
+  let sum = 0
+  for (let i = 0; i < _array.length; i++) {
+    sum = sum + _array[i]
+  }
+
+  return sum
+}
+
+function averg (_array) {
+  return adder_infinite(_array) / _array.length
+}
+
+function updateResults (className) {
+  let array = getArrayOfSelectedNumbers(className)
+  let selected = array.join(',')
+  let amount = array.length
+  let sum = adder_infinite(array)
+  let average = roundString(averg(array), 1)
+  document.querySelector('#selected span').innerHTML = selected
+  document.querySelector('#amount span').innerHTML = amount
+  document.querySelector('#sum span').innerHTML = sum
+  document.querySelector('#average span').innerHTML = average
+}
 
 /*
 
@@ -130,7 +168,7 @@ to use the correct argument when calling it.
 
 Note that updating the results actually is changing the HTML-content of the <spans>.
 Check index.html to see what I mean by "<spans>".
-Hopefully you remember by now tha twe use innerHTML to update the HTML-content of an
+Hopefully you remember by now that we use innerHTML to update the HTML-content of an
 HTML-element.
 
 
@@ -179,8 +217,10 @@ below to always show a number that has one decimal.
 
 */
 
-function roundString(numberWithManyDecimals, decimals){
+function roundString (numberWithManyDecimals, decimals) {
   // From: https://stackoverflow.com/a/12698296/2027283
-  var rounded = Math.pow(10, decimals);
-  return (Math.round(numberWithManyDecimals * rounded) / rounded).toFixed(decimals);
+  var rounded = Math.pow(10, decimals)
+  return (Math.round(numberWithManyDecimals * rounded) / rounded).toFixed(
+    decimals
+  )
 }
