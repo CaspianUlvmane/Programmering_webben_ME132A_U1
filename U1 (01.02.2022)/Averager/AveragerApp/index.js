5e10197ad7b1e905cc2ff37f069cab5e1948c225
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-02-08 19:19:42 +0100

    Link_to_repository

--- U1 (01.02.2022)/Averager/AveragerApp/index.js
+++ U1 (01.02.2022)/Averager/AveragerApp/index.js
@@ -4,7 +4,7 @@
 // Don't forget to include your CSS-file in the folder
 
 // Include the link to your Github Repository here:
-// Link:
+// Link: https://github.com/CaspianUlvmane/Programmering_webben_ME132A_U1.git 
 
 
 
